36c5e5c12a156de0b970640c1f5326483f5ea074
RileyJRaso <riley.j.raso@gmail.com>
2021-11-30 15:41:34 -0800

    Added features:  - Used matplotlib.pyplot to display data in a more user friendly way  - created Create_Array function to prepare data in a way matplotlib.pyplot could read  - changed limit to top 10 for better plot at the end  - used function Display_Data() to create matplotlib.pyplot plot

--- Query_Script.py
+++ Query_Script.py
@@ -1,5 +1,5 @@
 from google.cloud import bigquery
-
+import matplotlib.pyplot as plt
 
 def query_table(querystring):
     BQclient = bigquery.Client()
@@ -10,15 +10,33 @@ def query_table(querystring):
 
     return results
 
+def Create_Array(results):
+    Names = []
+    Values = []
+
+    for row in results:
+        Names.append(row.name)
+        Values.append(row.Total_Number_of_Repos)
+
+    Results_In_Array = [Names, Values]
+
+    return Results_In_Array
+
+def Display_Data(names, values):
+    plt.bar(names, values)
+    plt.show()
+
 if __name__ == "__main__":
+
     querystring = """
                   SELECT L.name, COUNT(*) AS `Total_Number_of_Repos`
                   FROM `bigquery-public-data.github_repos.languages`,
                   UNNEST(language) as L GROUP BY L.name
                   ORDER BY Total_Number_of_Repos DESC
-                  LIMIT 1000
+                  LIMIT 10
                   """
     results = query_table(querystring)
 
-    for row in results:
-        print("{} Has {} repos on Github".format(row.name, row.Total_Number_of_Repos))
+    Fixed_Results = Create_Array(results)
+
+    Display_Data(Fixed_Results[0], Fixed_Results[1])
